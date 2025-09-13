5cd92523a4ae0096492771350efc02efa3d3cbb2
NatalieBern <bernatalie09@gmail.com>
2025-09-13 08:54:44 +0300

    input added

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -66,7 +66,7 @@ public class Movement : MonoBehaviour
     public bool Occupied(Vector2 direction) 
     {
         RaycastHit2D hit = Physics2D.BoxCast(this.transform.position, Vector2.one * 0.75f, 0.0f, direction, 1.5f, this.obstacleLayer);
- 
+       
         return hit.collider != null;
     }
 
